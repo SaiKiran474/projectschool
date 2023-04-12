3a06554af1fe2eb835714ce3719737972b024e93
SaiKiran474 <saikiranchowdary474@gmail.com>
2023-04-12 18:05:40 +0530

    Delete hello_world.sol

--- hello_world.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier:MIT
-pragma solidity 0.8.7;
-contract hello_world{
-    // string a=hello
-    function get() public view returns(string memory){
-        return 'Hello Sai Kiran';
-    }
-        
-}
