94b9bcc852088645212343238c16b3a16fe4927f
SaiKiran474 <saikiranchowdary474@gmail.com>
2023-04-12 18:07:28 +0530

    Update sum.sol

--- sum.sol
+++ sum.sol
@@ -1,14 +1,14 @@
 //SPDX-License-Identifier:MIT
 pragma solidity >=0.8.7;
 contract sum{
-    int a;
+    int a1;
     int b;
     function set(int x,int y) public{
-        a=x;
+        a1=x;
         b=y;
     }
     function get() public view returns (int){
-        return a+b;
+        return a1+b;
 
     } 
     
